42e6252faf41d961a15e29e5ed61fb2376a0eae6
Gowtham <gowtham@appranix.com>
2017-11-02 18:12:29 +0530

    Initial Commit

--- build.py
+++ build.py
@@ -9,15 +9,16 @@ use_plugin("python.distutils")
 
 
 name = "hello-app-flask"
-default_task = "publish"
+default_task = ["analyze", "publish"]
 
 
 @init
 def set_properties(project):
+    
+    project.set_property("flake8_exclude_patterns", "setup.py,/target,htmlcov,devenv")
     project.set_property("dir_source_main_python", "src/main/python/helloapp")
     project.set_property("dir_source_unittest_python", "src/unittest/python/helloapp")
     project.depends_on('flask')
-    project.build_depends_on('coverage')
     project.set_property('coverage_break_build', True)
     project.set_property('flake8_break_build', True)
     project.set_property('flake8_verbose_output', True)
--- src/main/python/helloapp/webapp.py
+++ src/main/python/helloapp/webapp.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 
 application = Flask(__name__)
 
+
 @application.route('/')
 def index():
-    
+
     application.logger.info('/')
     # Render default page template
     return render_template('index.html')
