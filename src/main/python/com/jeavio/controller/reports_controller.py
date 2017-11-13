a21e1f9abc89d44a3470168774000ffd2e4bdc44
user <Karthikeyan>
2017-11-13 09:59:26 +0530

    Changes made in starting script

--- src/main/python/com/jeavio/controller/reports_controller.py
+++ src/main/python/com/jeavio/controller/reports_controller.py
@@ -4,6 +4,7 @@ Created on 03-Nov-2017
 @author: karthikeyan
 '''
 from flask import Flask
+from flask import jsonify
 from com.jeavio.service.report_service import ReportService
 
 app = Flask("SystemAnalyzer")
@@ -16,14 +17,14 @@ app = Flask("SystemAnalyzer")
 @app.route("/report/cpu")
 def get_cpu_report():
     reportService = ReportService()
-    return str(reportService.get_all_cpu_report())
+    return jsonify(reportService.get_all_cpu_report())
 
 
 @app.route("/report/virtualmemory")
 def get_virtual_memory_report():
     reportService = ReportService()
 #     return json.dumps(reportService.get_all_ram_report().__dict__)
-    return str(reportService.get_all_ram_report())
+    return jsonify(reportService.get_all_ram_report())
 
 
 def start_flask():
